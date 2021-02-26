9f4ceb203935121cc5959da133332970f299d207
Vissu-A <37015667+Vissu-A@users.noreply.github.com>
2021-02-26 09:59:09 +0530

    modified main.tf file

--- main.tf
+++ main.tf
@@ -6,4 +6,19 @@ provider "aws" {
 resource "aws_instance" "Test_Terraform" {
   ami           = "ami-09246ddb00c7c4fef"
   instance_type = "t2.micro"
+
+  provisioner "remote-exec" {
+    inline = ["sudo apt update", "sudo apt install python3 -y", "echo Done!"]
+    connection {
+      host         = aws_instance.Test_Terraform.public_ip
+      type        = "ssh"
+      user        = "root"
+      private_key = "${file("~/.ssh/id_rsa")}"
+    }
+  }
+  
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i 'aws_instance.Test_Terraform.public_ip,' --private-key /etc/ansible/vissu_aws_private.pem' /etc/ansible/playbooks/install_httpd.yml"
+  }
+
 }
